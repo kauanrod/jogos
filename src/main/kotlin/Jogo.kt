ede2a92f90e76602da4a09c150a4817c1c137b38
kauanrod <kauanrod@hotmail.com>
2023-09-26 11:20:19 -0300

    First commit

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,14 @@
+data class Jogo(
+  val titulo: String,
+  val capa: String
+) {
+  val descricao = ""
+  override fun toString(): String {
+    return "Meu jogo: \n" +
+            "Título: $titulo \n" +
+            "Capa: $capa \n" +
+            "Descrição: ${this.descricao}"
+  }
+
+
+}
